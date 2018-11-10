bd8facff44432269645d789eec9c4d9fb7dbbbf1
Juan F Gonzalez <juano_gonza@hotmail.com>
2018-11-09 22:34:29 -0500

    Modified hour display

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Data({hour, index}) {
     return (
         <tr>
-            <td>{index + 8} </td>
+            <td>{index + 8 + ":00"} </td>
             <td>{hour.Lunes} </td>
             <td>{hour.Martes} </td>
             <td>{hour.Miercoles} </td>
